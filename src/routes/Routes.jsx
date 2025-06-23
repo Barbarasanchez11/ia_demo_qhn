3f9e92e6ff1c7bb9442a13589d4d85041725ae4d
Barbarasanchez11 <167989308+Barbarasanchez11@users.noreply.github.com>
2025-06-23 10:38:49 +0200

    feat:Update project structure and cleanup files

---
+++ src/routes/Routes.jsx
@@ -0,0 +1,11 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+const Routes = () => {
+    return (
+        <Router>
+            <Routes />
+        </Router>
+    )
+}
+
+export default Routes;
